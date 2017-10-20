f6f883531950fe7e8b147b2b7c3c53dc2e35108d
micd84 <michal.s.debski@gmail.com>
2017-10-20 16:04:19 +0200

    Added input form data binding and validation

--- PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PartyInvites.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,7 +9,6 @@ namespace PartyInvites.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: Home
         public ViewResult Index()
         {
             int hour = DateTime.Now.Hour;
@@ -18,10 +18,24 @@ namespace PartyInvites.Controllers
             return View();
         }
 
+        [HttpGet]
         public ViewResult RsvpForm()
         {
 
             return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                return View();
+            }
         }
     }
 }
